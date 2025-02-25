e9fb9149a24aee5f34e85907f9c52d37bb763803
pereira299 <lucasper057@gmail.com>
2025-02-25 11:01:33 -0300

    update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,12 +18,8 @@ export default function Home() {
         />
       </Link>
       <section className="flex flex-col lg:flex-row lg:justify-between gap-4 px-4 py-8 bg-white">
-        <div className="w-full lg:w-1/2 flex flex-col gap-5">
-          <h3 className="text-brand font-extrabold text-4xl">
-            Escolha sua forma de contribuição!
-          </h3>
+        <div className="w-full lg:w-1/2 flex flex-col gap-5 justify-center">
           <div className="flex flex-row justify-between items-center gap-2">
-            
             <Link
               href="https://goingressos.com.br/acesso/login-evangelizar"
               className="border w-full py-5 flex-col border-slate-300 flex items-center justify-center px-4 rounded-md text-brand"
@@ -32,39 +28,39 @@ export default function Home() {
             </Link>
           </div>
           <div>
-            <p className="text-lg">
+            <p className="text-lg text-center">
               Configure o pagamento recorrente e contribua mensalmente com a
               evangelização.
             </p>
           </div>
         </div>
         <div className="w-full lg:w-1/2">
-          <h4 className="text-brand font-extrabold text-4xl">
-          Conheça nossas formas de contribuição e seus benefícios
+          <h4 className="text-brand text-4xl">
+            Conheça nossos planos e <b className="font-extrabold">APOIE</b>
           </h4>
           <ul className="flex flex-col gap-4">
             <li className="border border-slate-300 rounded-md p-4">
-              <h6 className="flex flex-row items-center gap-2 text-brand">
-                <Calendar size={24} />
-                <span className="text-xl font-bold">Aniversariante do Mês</span>
+              <h6 className="flex flex-row items-center gap-2 text-brand text-xl font-bold">
+                R$ 15,00 - Plano Ofertante
               </h6>
-              <p>Sorteios de um bolo para o aniversariante do mês.</p>
+              <p>Fique por dentro de todas as novidades</p>
             </li>
             <li className="border border-slate-300 rounded-md p-4">
-              <h6 className="flex flex-row items-center gap-2 text-brand">
-                <Church size={24} />
-                <span className="text-xl font-bold">Missas Mensais</span>
-              </h6>
-              <p>Missas mensais presididas pelo Pe. Luiz Senigalia</p>
-            </li>
-            <li className="border border-slate-300 rounded-md p-4">
-              <h6 className="flex flex-row items-center gap-2 text-brand">
-                <Plane size={24} />
-                <span className="text-xl font-bold">Sorteios de viagens</span>
+              <h6 className="flex flex-row items-center gap-2 text-brand text-xl font-bold">
+                R$ 30,00 - Plano Missionário
               </h6>
               <p>
-                Sorteios de viagens exclusivas para o Santuário Nacional e
-                outros prémios incríveis.
+                Receba uma de nossas camisetas em seu aniversário e tenha acesso
+                exclusivo a pré-venda de nossos eventos
+              </p>
+            </li>
+            <li className="border border-slate-300 rounded-md p-4">
+              <h6 className="flex flex-row items-center gap-2 text-brand text-xl font-bold">
+                R$ 60,00 - Plano Evangelizar SEMPRE
+              </h6>
+              <p>
+                Receba um box especial a cada 2 meses e tenha acesso gratuito e
+                exclusivo a nossos eventos
               </p>
             </li>
           </ul>
